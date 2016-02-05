42d5c72e2efc1ffb45857a6cb7aa46c7414bff98
Yichung Hsu <fov223@gmail.com>
2016-02-05 13:22:11 +0800

    cost funtion and its gradient built

--- script.py
+++ script.py
@@ -4,6 +4,37 @@ import numpy as np
 
 import datetime
 
+def CostFunction(x, Y, R, n_movies, n_genres, C):
+    X, Theta = transformTo2Dparams(x, n_movies, n_genres)
+    regularization_term = sum(x**2)/(2*C)
+    J = (((R*(np.dot(X, Theta)-Y)))**2).sum()/2 + regularization_term
+    return J
+
+def gradientCostFunction(x, Y, R, n_movies, n_genres, C):
+    X, Theta = transformTo2Dparams(x, n_movies, n_genres)
+    dX     = np.empty(X.shape)
+    dTheta = np.empty(Theta.shape)
+
+    tmp = R*(np.dot(X, Theta)-Y)
+    for k in range(n_genres):
+        for i in range(n_movies):
+            dX[i,k]     = np.dot(tmp[i, :], Theta[k,:]) + X[i, k]
+        for j in range(n_users):
+            dTheta[k,j] = np.dot(tmp[:,j], X[:,k])      + Theta[k, j]
+    dx = transformTo1Dparams(dX, dTheta)
+    return dx
+    
+def transformTo1Dparams(X, Theta):
+    return np.concatenate((np.asarray(X).reshape((1,-1))[0], Theta.reshape((1,-1))[0]))
+
+def transformTo2Dparams(x, n_movies, n_genres):
+    X     = x[:n_movies*n_genres].reshape((-1,n_genres))
+    Theta = x[n_movies*n_genres:].reshape((n_genres,-1))
+    return X, Theta
+
+def similarityOfMovies(mid_i, mid_j, X):
+    return 1 / float(sum( (X.iloc[mid_i,:]-X.iloc[mid_j,:])**2 ))
+
 movie = pd.read_table('ml-100k/u.item', sep='|', header=None)
 
 train = pd.read_table('ml-100k/u1.base', header=None, names=['uid', 'mid', 'rating', 'timestamp'])
@@ -15,13 +46,16 @@ n_users  = int(train.describe().loc['max', 'uid'])
 n_movies = int(train.describe().loc['max', 'mid'])
 n_genres = movie.iloc[:,5:].shape[1]
 
-ones = pd.DataFrame( np.ones(n_movies) )
 X = movie.iloc[:,5:]
-X = pd.concat([ones, X], axis=1)
 R = np.zeros( (n_movies, n_users) )
 Y = R
 for i in range(train.shape[0]):
     R[train.mid[i]-1][train.uid[i]-1]=1
     Y[train.mid[i]-1][train.uid[i]-1]=train.rating[i]
 
-Theta = np.zeros( (n_genres+1, n_users) )
+Theta = np.random.rand( n_genres, n_users )
+
+# Optimization Process (CG)
+x = transformTo1Dparams(X, Theta)
+C = 5
+print CostFunction(x, Y, R, n_movies, n_genres, C)
