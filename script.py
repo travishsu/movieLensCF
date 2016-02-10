e127f8034f73620bf9f1655d30fb73e290e23ba9
Yichung Hsu <fov223@gmail.com>
2016-02-10 14:55:01 +0800

    nobug

--- script.py
+++ script.py
@@ -16,9 +16,6 @@ for i in range(train.shape[0]):
     R[train.mid[i]-1][train.uid[i]-1] = 1
 
 Y_mu = Y.sum(axis=1)/R.sum(axis=1)
-# Fill mean of each movie to no-rating
-for i in range(n_movies):
-    Y[np.ix_([i],[k for k in range(n_users) if R[i,k]==0])] = Y_mu[i]
 
 # Allocate
 weights    = np.empty((n_movies,n_movies))
@@ -27,25 +24,25 @@ std_rating = np.empty(n_movies)
 
 for i in range(n_movies):
     diff[i,:] = Y[i,:]-Y_mu[i]
-    std_rating[i] = diff[i,:].std()
+    std_rating[i] = np.power(diff[i,:],2).sum()/R[i,:].sum()
 
 # Weighting (Pearson correlation)
 for i in range(n_movies):
     for j in range(n_movies):
-        weights[i,j] = np.dot(diff[i,:], diff[j,:])/(std_rating[i]*std_rating[j])
+        weights[i,j] = np.dot(R[i,:]*diff[i,:], R[j,:]*diff[j,:])/(std_rating[i]*std_rating[j])
 
 # Predict
 pred_correlation = np.empty(test.shape[0])
 pred_cosine      = pred_correlation.copy()
-thredhold = 800
+thredhold = 0.00001
 for test_id in range(test.shape[0]):
     a = test.uid[test_id]
     i = test.mid[test_id]
 
-    idx_positive_weights = np.array([j for j in range(weights.shape[0]) if weights[i,j]>thredhold])
-    val_positive_weights = weights[np.ix_([i],idx_positive_weights)][0]
-    dif_positive_weights = (diff[np.ix_(idx_positive_weights,[a])].T)[0]
-    pred_correlation[test_id] = Y_mu[i] + np.dot(dif_positive_weights,val_positive_weights)/(val_positive_weights.sum())
+    idx_positive_weights    = np.array([j for j in range(weights.shape[0]) if weights[i,j]>thredhold])
+    val_positive_weights    = weights[np.ix_([i],idx_positive_weights)][0]
+    val_positive_difference = (diff[np.ix_(idx_positive_weights,[a])].T)[0] * (R[np.ix_(idx_positive_weights,[a])].T)[0]
+    pred_correlation[test_id] = Y_mu[i] + np.dot(val_positive_difference,val_positive_weights)/(val_positive_weights.sum())
 
 # Validation
 error   = test.rating - pred_correlation
