cb503dfdbf99b34c70c09254a9c392ce2b213d3b
Yichung Hsu <fov223@gmail.com>
2016-02-10 15:43:13 +0800

    update to item-based

--- script.py
+++ script.py
@@ -1,60 +1,51 @@
-
 import pandas as pd
 import numpy as np
-from scipy import optimize
 
-import datetime
-
-def transformTo1Dparams(X, Theta):
-    return np.concatenate((np.asarray(X).reshape((1,-1))[0], Theta.reshape((1,-1))[0]))
-
-def transformTo2Dparams(x, n_movies, n_genres):
-    X     = x[:n_movies*n_genres].reshape((-1,n_genres))
-    Theta = x[n_movies*n_genres:].reshape((n_genres,-1))
-    return X, Theta
-
-def CostFunction(x, Y, R, n_movies, n_genres, C):
-    X, Theta = transformTo2Dparams(x, n_movies, n_genres)
-    regularization_term = sum(np.power(x,2))/(2*C)
-    return (np.power((R*(np.dot(X, Theta)-Y)),2)).sum()/2 + regularization_term
-
-def gradientCostFunction(x, Y, R, n_movies, n_genres, C):
-    X, Theta = transformTo2Dparams(x, n_movies, n_genres)
-    dX     = np.empty(X.shape)
-    dTheta = np.empty(Theta.shape)
-
-    tmp = R*(np.dot(X, Theta)-Y)
-    dX     = np.dot(tmp, Theta.T) + X/C
-    dTheta = np.dot(X.T, tmp)     + Theta/C
-    return transformTo1Dparams(dX, dTheta)
-
-def similarityOfMovies(mid_i, mid_j, X):
-    return 1 / float(sum( np.power(X.iloc[mid_i,:]-X.iloc[mid_j,:],2) ))
-
-movie = pd.read_table('ml-100k/u.item', sep='|', header=None)
-
-train = pd.read_table('ml-100k/u1.base', header=None, names=['uid', 'mid', 'rating', 'timestamp'])
-test  = pd.read_table('ml-100k/u1.test', header=None, names=['uid', 'mid', 'rating', 'timestamp'])
-
-train.timestamp = [datetime.datetime.fromtimestamp(int(s)) for s in train.timestamp]
+# Load data
+train = pd.read_table('ml-100k/u2.base', header=None, names=['uid', 'mid', 'rating', 'timestamp'])
+test  = pd.read_table('ml-100k/u2.test', header=None, names=['uid', 'mid', 'rating', 'timestamp'])
 
 n_users  = int(train.describe().loc['max', 'uid'])
 n_movies = int(train.describe().loc['max', 'mid'])
-n_genres = movie.iloc[:,5:].shape[1]
 
-X = movie.iloc[:,5:]
-R = np.zeros( (n_movies, n_users) )
-Y = R
+# Transform
+Y = np.zeros( (n_movies, n_users) )
+R = Y.copy()
 for i in range(train.shape[0]):
-    R[train.mid[i]-1][train.uid[i]-1]=1
-    Y[train.mid[i]-1][train.uid[i]-1]=train.rating[i]
+    Y[train.mid[i]-1][train.uid[i]-1] = train.rating[i]
+    R[train.mid[i]-1][train.uid[i]-1] = 1
 
-Theta = np.random.rand( n_genres, n_users )
+Y_mu = Y.sum(axis=1)/R.sum(axis=1)
 
-# Optimization Process (CG)
-x0 = transformTo1Dparams(X, Theta)
-C = 100
-print CostFunction(x0, Y, R, n_movies, n_genres, C)
-result = optimize.fmin_cg( (lambda x: CostFunction(x, Y, R, n_movies, n_genres, C)), x0,
-           fprime=(lambda x: gradientCostFunction(x, Y, R, n_movies, n_genres, C)),
-           full_output=True, retall=True, disp=True)
+# Allocate
+weights    = np.empty((n_movies,n_movies))
+diff       = np.empty((n_movies, n_users))
+std_rating = np.empty(n_movies)
+
+for i in range(n_movies):
+    diff[i,:] = Y[i,:]-Y_mu[i]
+    std_rating[i] = np.power(diff[i,:],2).sum()/R[i,:].sum()
+
+# Weighting (Pearson correlation)
+for i in range(n_movies):
+    for j in range(n_movies):
+        weights[i,j] = np.dot(R[i,:]*diff[i,:], R[j,:]*diff[j,:])/(std_rating[i]*std_rating[j])
+
+# Predict
+pred_correlation = np.empty(test.shape[0])
+pred_cosine      = pred_correlation.copy()
+thredhold = 0.00001
+for test_id in range(test.shape[0]):
+    a = test.uid[test_id]
+    i = test.mid[test_id]
+
+    idx_positive_weights    = np.array([j for j in range(weights.shape[0]) if weights[i,j]>thredhold])
+    val_positive_weights    = weights[np.ix_([i],idx_positive_weights)][0]
+    val_positive_difference = (diff[np.ix_(idx_positive_weights,[a])].T)[0] * (R[np.ix_(idx_positive_weights,[a])].T)[0]
+    pred_correlation[test_id] = Y_mu[i] + np.dot(val_positive_difference,val_positive_weights)/(val_positive_weights.sum())
+
+# Validation
+error   = test.rating - pred_correlation
+correct = np.array([error[i] for i in range(test.shape[0]) if np.abs(error[i])<0.5])
+correct_proportion = correct.shape[0]/float(test.shape[0])
+print correct_proportion
