0bd5eb2a3abfecbaddd89aeee09a357f845979e3
Yichung Hsu <fov223@gmail.com>
2016-02-17 08:28:01 +0800

    compare K

--- script.py
+++ script.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import numpy as np
-<<<<<<< HEAD
 import matplotlib.pyplot as plt
 
 # Load data
@@ -18,6 +17,9 @@ for i in range(train.shape[0]):
     R[train.mid[i]-1][train.uid[i]-1] = 1
 
 Y_mu = Y.sum(axis=1)/R.sum(axis=1)
+nanindex = [l for l in range(n_movies) if np.isnan(Y_mu[l])]
+for i in nanindex:
+    Y_mu[nanindex] = 0
 
 # Allocate
 weights    = np.empty((n_movies,n_movies))
@@ -26,26 +28,30 @@ std_rating = np.empty(n_movies)
 
 for i in range(n_movies):
     diff[i,:] = Y[i,:]-Y_mu[i]
-    std_rating[i] = np.power(diff[i,:],2).sum()/R[i,:].sum()
+    std_rating[i] = np.power(diff[i,:],2).sum()/(R[i,:].sum() if R[i,:].sum()>0 else 1)
 
 # Weighting (Pearson correlation)
 for i in range(n_movies):
     for j in range(n_movies):
-        weights[i,j] = np.dot(R[i,:]*diff[i,:], R[j,:]*diff[j,:])/(std_rating[i]*std_rating[j])
+        if std_rating[i]*std_rating[j]!=0:
+            weights[i,j] = np.dot(R[i,:]*diff[i,:], R[j,:]*diff[j,:])/(std_rating[i]*std_rating[j])
+        else:
+            weights[i,j] = 0
 
 # Predict
 pred_correlation = np.empty(test.shape[0])
 pred_flag        = pred_correlation.copy()
 pred_cosine      = pred_correlation.copy()
-thredhold = 0.00001
+K         = 50
 tolerence = 1
 for test_id in range(test.shape[0]):
     a = test.uid[test_id]
     i = test.mid[test_id]
 
-    idx_positive_weights    = np.array([j for j in range(weights.shape[0]) if weights[i,j]>thredhold])
-    val_positive_weights    = weights[np.ix_([i],idx_positive_weights)][0]
-    val_positive_difference = (diff[np.ix_(idx_positive_weights,[a])].T)[0] * (R[np.ix_(idx_positive_weights,[a])].T)[0]
+    sort_index = np.argsort(-weights[i,:])
+    idx_positive_weights    = np.array(sort_index[:K])
+    val_positive_weights    = weights[i,idx_positive_weights]
+    val_positive_difference = (diff[dx_positive_weights,a]) * (R[idx_positive_weights,a])
     pred_correlation[test_id] = Y_mu[i] + np.dot(val_positive_difference,val_positive_weights)/(val_positive_weights.sum())
 
     pred_flag[test_id] = 1
@@ -63,7 +69,6 @@ plt.ylabel("Frequency")
 plt.title("Item-based CF")
 print correct_proportion
 plt.show()
-=======
 
 # Load data
 train = pd.read_table('ml-100k/u2.base', header=None, names=['uid', 'mid', 'rating', 'timestamp'])
